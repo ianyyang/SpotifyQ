6bc6e7c95c5eeae3c4b7d1e8c76913f0767de459
Sujith <sujithc248@gmail.com>
2019-10-14 20:49:14 -0400

    Added more functionality to room setup

--- client/src/components/hostroom.js
+++ client/src/components/hostroom.js
@@ -3,8 +3,50 @@ import React, { Component } from 'react';
 class HostRoom extends Component {
     constructor(){
         super();
-        this.state = {...this.props}
+        this.state = {
+            ...this.props,
+            nowPlaying: { 
+                name: '', 
+                image: '',
+            }
+        }
     }
+
+    getNowPlaying(){
+        this.props.spotifyWebApi.getMyCurrentPlaybackState()
+        .then((response) => {
+          console.log('Now playing successfully captured!', response);
+    
+          this.setState({
+            nowPlaying: {
+              name:response.item.name,
+              image: response.item.album.images[0].url
+            }
+          })
+        }, function(err) {
+          console.error('Something went wrong!', err);
+        });
+      }
+
+      /*resumePlayback(){
+        var index = document.getElementById('devices').selectedIndex;
+        spotifyWebApi.play({device: this.state.devices[index].id})
+        .then(function(data) {
+          console.log('Playback resumed!', data);
+        }, function(err) {
+          console.error('Something went wrong!', err);
+        })
+      }
+    
+      pausePlayback() {
+        var index = document.getElementById('devices').selectedIndex;
+        spotifyWebApi.pause({device: this.state.devices[index].id})
+        .then(function(data) {
+          console.log('Playback paused!');
+        }, function(err) {
+          console.error('Something went wrong!', err);
+        });
+      }*/
 
     handleClick(compName, e){
         console.log(compName);
@@ -13,7 +55,18 @@ class HostRoom extends Component {
     _renderSubComp(){
         switch(this.state.render){
             case 'new': return <div>this is the host room</div>
-            default: return null;
+            default: return ( 
+            <div>   
+                <h1>{this.props.userInfo.display_name}'s Room</h1>
+                <label>Access Code: </label> 
+                <div> Now Playing: {this.state.nowPlaying.name}</div><div>
+                <img src={this.state.nowPlaying.image } style={{width:100}}/>
+                </div>
+                <button onClick={() => this.getNowPlaying()}>
+                    Check Now Playing
+                </button>
+            </div>
+            );
         }
     }
 
@@ -26,5 +79,6 @@ class HostRoom extends Component {
       );
     }
   }
+  
   
 export default HostRoom;
--- client/src/components/newroom.js
+++ client/src/components/newroom.js
@@ -1,35 +1,135 @@
 import React, { Component } from 'react';
 
-import AppFunc from '../App-function'
+import HostRoom from './hostroom'
 
 
 class NewRoom extends Component {
-    
-        state = {
-            ...this.props
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...this.props,
+            newFlag: false,
+            newPlaylist: "New Playlist",
+            playlists:[], 
+            selectedPlaylist:'',
+            selectedDevice: ''
         };
 
+      }
     
+        
+    postNewPlaylist(){
+        this.props.spotifyWebApi.createPlaylist(this.props.userInfo.id, {'name': this.state.newPlaylist})
+        .then((response) => {
+            //alert('New playlist created: ', response.name);
+            this.state.playlists.push(response);
+        }, function(err) {
+            console.error('Something went wrong!', err);
+        })
+    }
+
+    getUserPlaylists(){
+        this.state.spotifyWebApi.getUserPlaylists()
+        .then((response) => {
+          console.log('User playlists successfully captured!', response);
+          
+          var json = response.items;
+          var arr = [];
+    
+          Object.keys(json).forEach(function(key) {
+            arr.push({
+                name: json[key].name,
+                id: json[key].id
+              })
+          });
+    
+          this.setState({
+            playlists: arr
+          }, function(err) {
+            console.error('Something went wrong!', err);
+          });
+        })
+      }
+
+    postAndGet(){
+        this.postNewPlaylist();
+        //this.getUserPlaylists();
+        this.state.newFlag = true;
+        window.location.reload();
+    }
+    
+    buildListOfPlaylists = function(){
+        if (this.state.newFlag){
+            return this.state.playlists;
+        }else{
+            return this.props.playlists;
+        }
+
+    }
 
     handleClick(compName, e){
         console.log(compName);
         this.setState({render:compName});        
     }
+
+    handleChange(event) {
+        this.setState({newPlaylist: event.target.value});
+      }
+      
+    handlePlaylistChange = (event) => {
+        /*alert("test");
+        var index = event.nativeEvent.target.selectedIndex;
+        if (this.state.newFlag){
+            this.setState({selectedPlaylist: this.state.playlists[index]});
+        }else{
+            this.setState({selectedPlaylist: this.props.playlists[index]});
+        }*/
+
+    }
+
+    /*validateAndNext() {
+        var d_index = document.getElementById('devices').selectedIndex;
+        var p_index = document.getElementById('playlists').selectedIndex;
+
+        this.setState({selectedDevice: this.props.devices[d_index]});
+
+        if (this.state.newFlag){
+            this.setState({selectedPlaylist: this.state.playlists[p_index]});
+        }else{
+            this.setState({selectedPlaylist: this.props.playlists[p_index]});
+        }
+
+        this.handleClick.bind(this, 'next')
+      }*/
+
     _renderSubComp(){
         switch(this.state.render){
-            case 'next': return <AppFunc/>
+            case 'next': return <HostRoom {...this.props}/>
+
             default: return (
             <div>
-                <h1>Room Hosted By: {this.props.userInfo.display_name}</h1>
-                <ul>Choose Device:</ul>
+                <h1>New Room Hosted By: {this.props.userInfo.display_name}</h1>
+                <label>Choose Device: <br/>
                 <select id="devices">{this.props.devices.map(this.MakeItem)}</select>
+                </label>
 
                 <div>
-                <ul>Choose Playlist:</ul>
-                    <select>{this.props.playlists.map(this.MakeItem)}</select>
+                    <label>
+                    <br/> Create New Playlist: <br/>
+                    <input type="text" value={this.state.newPlaylist} onChange={this.handleChange.bind(this)} />
+                    </label>
+                    <input onClick={() => this.postAndGet()} type="submit" value="Submit" /> 
                 </div>
 
+                <div>
+                <label>Choose Room Playlist:<br/>
+                    <select id="playlists">{this.buildListOfPlaylists().map(this.MakeItem)}} onChange={this.handlePlaylistChange.bind(this)}</select>
+                </label>
+                </div>
+                
                 <button onClick={this.handleClick.bind(this, 'next')}>Next</button> 
+
+
             </div>
                 );
         }
